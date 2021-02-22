d0dbc66bbdd50ad9dede09f7f52b566c67526d61
agneshew <agnes.hewusz@gmail.com>
2021-02-22 09:29:59 +0100

    change database to H2

---
+++ src/main/java/com/agnes/Library/config/WebMvcConfig.java
@@ -0,0 +1,8 @@
+package com.agnes.Library.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+}
--- src/main/java/com/agnes/Library/model/Book.java
+++ src/main/java/com/agnes/Library/model/Book.java
@@ -6,7 +6,6 @@ import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Getter
